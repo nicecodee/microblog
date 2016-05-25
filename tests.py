1b6bfec7a8faa2a3401e15bf816c2613f4333c14
Ubuntu <ubuntu@ip-172-31-31-230.ap-northeast-1.compute.internal>
2016-05-25 04:10:36 +0000

    part-7-unit-testing-complete

---
+++ tests.py
@@ -0,0 +1,40 @@
+import os
+import unittest
+
+from config import basedir
+from __init__ import app, db
+from models import User
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
+        self.app = app.test_client()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_avatar(self):
+        u = User(username='john', email='john@example.com')
+        avatar = u.avatar(128)
+        expected = 'http://www.gravatar.com/avatar/d4c74594d841139328695756648b6bd6'
+        assert avatar[0:len(expected)] == expected
+
+    def test_make_unique_username(self):
+        u = User(username='john', email='john@example.com')
+        db.session.add(u)
+        db.session.commit()
+        username = User.make_unique_username('john')
+        assert username != 'john'
+        u = User(username=username, email='susan@example.com')
+        db.session.add(u)
+        db.session.commit()
+        username2 = User.make_unique_username('john')
+        assert username2 != 'john'
+        assert username2 != username
+
+if __name__ == '__main__':
+    unittest.main()
